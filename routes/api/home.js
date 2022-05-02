aa2703d4e961ebae84bab1a86c5cc90c0600a076
Heran Mekonnen <mekonnen.heran@gmail.com>
2022-05-02 19:44:44 -0400

    Added a validation + msg for values < 0

--- routes/api/home.js
+++ routes/api/home.js
@@ -43,11 +43,15 @@ for (let i= totalBottles; i>=0; i--){
 
     if (i >=1){
    
-     res.status(202).json({msg: `${i} bottles of milk on the wall, ${i} bottles of milk.Take one down, pass it around:` , link: `http://localhost:1000/bottles/${i-1}`})
+     res.status(202).json({msg: `${i} bottle(s) of milk on the wall, ${i} bottle(s) of milk.Take one down, pass it around:` , link: `http://localhost:1000/bottles/${i-1}`})
      
     }
+    
     else if (totalBottles<=0){
        res.status(202).json({msg: `and then there were ${i} Bottles of milk on the wall.`})
+   }
+   else{
+       res.status(400).json({msg: `Please enter a number greater than zero :) `})
    }
 }})
 
